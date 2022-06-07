e0a8d6fc763bc62a5963dfd4f5f93b96605a9a3f
MESSI <mehmet@sixthnet.com>
2022-06-07 14:29:36 +0800

    css-minimizer-webpack-plugin 压缩

--- webpackConfig/webpack.prod.js
+++ webpackConfig/webpack.prod.js
@@ -1,10 +1,10 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const { merge } = require("webpack-merge");
 const commonConfig = require("./webpack.common.js");
 const prodConfig = {
   mode: "production",
   devtool: "cheap-module-source-map", // "cheap-module-source-map"
-  plugins: [new MiniCssExtractPlugin()],
   module: {
     rules: [
       {
@@ -24,6 +24,19 @@ const prodConfig = {
       },
     ],
   },
+  optimization: {
+    minimizer: [
+      // 这将仅在生产模式下启用 CSS 优化。
+      // 如果您还想在开发中运行它，请将optimization.minimize选项设置为true
+
+      // 在 webpack@5 中，你可以使用 `...` 语法来扩展现有的 minimizer（即 `terser-webpack-plugin`），将下一行取消注释
+      // `...`,
+      new CssMinimizerPlugin({
+        parallel: true,
+      }),
+    ],
+  },
+  plugins: [new MiniCssExtractPlugin()],
   output: {
     filename: "[name].[contenthash].js",
     chunkFilename: "[name].[contenthash].chunk.js",
