7441492bfa8cb7e1bb5d34ff87a5b90f564d2974
MESSI <mehmet@sixthnet.com>
2022-06-10 17:14:11 +0800

    useParams，useSearchParams，Object.fromEntries(searchParams)

--- src/ListTow.tsx
+++ src/ListTow.tsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import { useEffect } from 'react';
+import { useParams, useSearchParams } from 'react-router-dom';
 
 function ListTow() {
-  return <h1>ListTow</h1>;
+  const { id } = useParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  useEffect(() => {
+    setSearchParams({
+      ...Object.fromEntries(searchParams),
+      ...{ fo: '30', statu: 'NICE' },
+    });
+    console.log('searchParams', Object.fromEntries(searchParams));
+  }, []);
+  return (
+    <div>
+      <h1>
+        ListId:
+        {id}
+      </h1>
+      <h1>
+        searchParams:
+        {searchParams.get('statu')}
+      </h1>
+    </div>
+  );
 }
 
 export default ListTow;
